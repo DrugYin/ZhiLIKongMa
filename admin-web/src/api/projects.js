bfd55bdf8335c76eae5924f5df2b43ab4f5f5293
LiChongWei <2209338565@qq.com>
2026-04-21 15:35:01 +0800

    feat: 完成后台项目配置增删改查

--- admin-web/src/api/projects.js
+++ admin-web/src/api/projects.js
@@ -1,9 +1,42 @@
 import { callAdminFunction } from './cloudbase';
 
 export function getProjects(params = {}) {
-  return callAdminFunction('admin-get-projects', params);
+  return callAdminFunction('admin-manage-projects', {
+    action: 'list',
+    ...params
+  });
+}
+
+export function getProjectDetail(projectCode) {
+  return callAdminFunction('admin-manage-projects', {
+    action: 'get',
+    project_code: projectCode
+  });
+}
+
+export function createProject(payload) {
+  return callAdminFunction('admin-manage-projects', {
+    action: 'create',
+    project: payload
+  });
 }
 
 export function updateProject(payload) {
-  return callAdminFunction('admin-update-project', payload);
+  return callAdminFunction('admin-manage-projects', {
+    action: 'update',
+    project: payload
+  });
+}
+
+export function deleteProject(payload) {
+  return callAdminFunction('admin-manage-projects', {
+    action: 'delete',
+    ...payload
+  });
+}
+
+export function seedDefaultProjects() {
+  return callAdminFunction('admin-manage-projects', {
+    action: 'seed_defaults'
+  });
 }
