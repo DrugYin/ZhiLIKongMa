763ae93a111f165c2216f8b0279f14951a6bca5f
LiChongWei <2209338565@qq.com>
2026-04-22 13:49:51 +0800

    feat: 完成后台任务管理

--- admin-web/src/api/tasks.js
+++ admin-web/src/api/tasks.js
@@ -1,7 +1,45 @@
 import { callAdminFunction } from './cloudbase';
 
 export function getTasks(params = {}) {
-  return callAdminFunction('admin-get-tasks', params);
+  return callAdminFunction('admin-manage-tasks', {
+    action: 'list',
+    ...params
+  });
+}
+
+export function getTaskDetail(taskId) {
+  return callAdminFunction('admin-manage-tasks', {
+    action: 'get',
+    task_id: taskId
+  });
+}
+
+export function createTask(payload) {
+  return callAdminFunction('admin-manage-tasks', {
+    action: 'create',
+    task: payload
+  });
+}
+
+export function updateTask(payload) {
+  return callAdminFunction('admin-manage-tasks', {
+    action: 'update',
+    task: payload
+  });
+}
+
+export function deleteTask(payload) {
+  return callAdminFunction('admin-manage-tasks', {
+    action: 'delete',
+    ...payload
+  });
+}
+
+export function getTaskSubmissions(params = {}) {
+  return callAdminFunction('admin-manage-tasks', {
+    action: 'submissions',
+    ...params
+  });
 }
 
 export function getSubmissions(params = {}) {
